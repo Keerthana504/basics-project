38d837275995eb8cfbff6eaa5aa995df2b230c14
Keerthana504 <130107749+Keerthana504@users.noreply.github.com>
2023-08-18 21:34:30 -0400

    Aug 18

--- public/index.js
+++ public/index.js
@@ -49,6 +49,46 @@ name3();
 
 //example for bind
 
+//example for this
+const shape = {
+  radius: 10,
+  diameter() {
+    return this.radius * 2;
+  },
+  perimeter: () => 2 * Math.PI * this.radius,
+};
+
+console.log(shape.diameter()); // 20 as this here is pointing to its current scope in a regular function
+console.log(shape.perimeter()); // NaN as this here is pointing to global scope in an arrow funtion
+
+// class methods
+class Chameleon {
+  static colorChange(newColor) {
+    this.newColor = newColor;
+    return this.newColor;
+  }
+
+  constructor({ newColor = "green" } = {}) {
+    this.newColor = newColor;
+  }
+}
+
+const freddie = new Chameleon({ newColor: "purple" }); // object creation, constructor is called first and has access to all the methods inside the class
+console.log(freddie.colorChange("orange")); // orange - only if there is return statement in the method , else undefined.
+
+// function prototyping
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+const member = new Person("Lydia", "Hallie");
+Person.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+console.log(member.getFullName());
+
 //quotes API
 
 fetch("https://type.fit/api/quotes")
